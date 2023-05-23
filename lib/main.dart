329ffa3fcdcbdb2f4aadd5e0303c5c9224a577cb
Eugene M <eugene.moschyn@gmail.com>
2023-05-23 01:20:40 -0700

    add(): getNext word function with notifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
-void main() {
+void main()
+{
     runApp(MyApp());
 }
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
     const MyApp({super.key});
 
     @override
-    Widget build(BuildContext context) {
+    Widget build(BuildContext context)
+    {
         return ChangeNotifierProvider(
             create: (context) => MyAppState(),
             child: MaterialApp(
@@ -28,14 +31,23 @@ class MyApp extends StatelessWidget {
 }
 
 
-class MyAppState extends ChangeNotifier {
+class MyAppState extends ChangeNotifier
+{
     var current = WordPair.random();
+
+    void getNext()
+    {
+        current = WordPair.random();
+        notifyListeners();
+    }
 }
 
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget
+{
     @override
-    Widget build(BuildContext context) {
+    Widget build(BuildContext context)
+    {
         var appState = context.watch<MyAppState>();
 
         return Scaffold(
