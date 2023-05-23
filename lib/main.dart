607382626d824fb711b2aba885c160c3cfb83382
Eugene M <eugene.moschyn@gmail.com>
2023-05-23 01:26:01 -0700

    extract(): word into separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyHomePage extends StatelessWidget
             body: Column(
                 children: [
                     Text('A random idea:'),
-                    Text(pair.asLowerCase),
+                    BigCard(pair: pair),
                     ElevatedButton(
                         onPressed: () {
                             appState.getNext();
@@ -65,5 +65,21 @@ class MyHomePage extends StatelessWidget
                 ],
             ),
         );
+    }
+}
+
+
+class BigCard extends StatelessWidget
+{
+    const BigCard({
+        super.key,
+        required this.pair,
+    });
+
+    final WordPair pair;
+
+    @override
+    Widget build(BuildContext context) {
+        return Text(pair.asLowerCase);
     }
 }
